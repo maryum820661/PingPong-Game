0befe754c06f6b0afd0fe019bb3453cfbac85002
maryum820661 <maryumsheikh43462@gmail.com>
2020-05-05 16:02:08 +0500

    code added

---
+++ ping pong game/Form1 (2).cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ping_pong_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        public Boolean Collision_Left(PictureBox obj)
+        {
+            if (obj.Location.X <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean Collision_Right(PictureBox obj)
+        {
+            if (obj.Location.X + obj.Width >= WorldFrame.Width)
+            {
+                return true;
+            }
+            return false;
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                case Keys.Up:
+                    Player_Down = false;
+                    Player_Up = true;
+                    break;
+                case Keys.S:
+                case Keys.Down:
+                    Player_Up = false;
+                    Player_Down = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                case Keys.Up:
+                    Player_Up = false;
+                    break;
+                case Keys.S:
+                case Keys.Down:
+                    Player_Down = false;
+                    break;
+            }
+        }
+        if (Player_Up && !Collision_Up(pb_Player))
+                {               
+                    pb_Player.Top -= Speed_Player;    
+private void pb_Ball_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+if (Player_Down && !Collision_Down(pb_Player))
+                {               
+                    pb_Player.Top += Speed_Player;
+                }
+public Boolean Collision_Player(PictureBox tar)
+{
+    if (tar.Bounds.IntersectsWith(pb_Player.Bounds))
+    {
+        PictureBox temp1 = new PictureBox();
+        temp1.Bounds = pb_Player.Bounds;
+        temp1.SetBounds(temp1.Location.X + temp1.Width, temp1.Location.Y, 1, 10);
+        if (tar.Bounds.IntersectsWith(temp1.Bounds))
+        {
+            BallForce = 3;
+            return true;
+        }
+        temp1.SetBounds(temp1.Location.X, temp1.Location.Y + 5, 1, 10);
+        if (tar.Bounds.IntersectsWith(temp1.Bounds))
+        {
+            BallForce = 2;
+            return true;
+        }
+        temp1.SetBounds(temp1.Location.X, temp1.Location.Y + 10, 1, 10);
+        if (tar.Bounds.IntersectsWith(temp1.Bounds))
+        {
+            BallForce = 1;
+            return true;
+        }
+        if (BallForce > 0)
+        {
+            pb_Ball.Top -= BallForce;
+        }
+        if (BallForce < 0)
+        {
+            pb_Ball.Top -= BallForce;
+        }
+        if (pb_Ball.Location.Y <= 1)
+        {
+            BallForce = ReverseInt(BallForce, true, true);
+        }
+        if (pb_Ball.Location.Y + pb_Ball.Height >= WorldFrame.Height - 1)
+        {
+            BallForce = ReverseInt(BallForce, true, false);
+        }
+        public int ReverseInt(int x, Boolean Force = false, Boolean Negative = false)
+{
+    if (Force)
+    {
+        if (Negative)
+        {
+            if (x > 0)
+            {
+                x = ~x + 1;
+            }
+        }
+        else
+        {
+            x = x - (x * 2);
+        }
+    }
+    else
+    {
+        if (x > 0)
+        {
+            x = x - (x * 2);
+        }
+        else
+        {
+            x = ~x + 1;
+        }
+    }
+    return x;
+}
+if (pb_Enemy.Location.Y + 28 < pb_Ball.Location.Y)
+                {   //Which is around 28 pixels below its Y coordinate
+                    pb_Enemy.Top += Speed_Enemy;
+                }
+                else
+                {
+                    pb_Enemy.Top -= Speed_Enemy;
+                }
+    public void AddScore(PictureBox[] Arr)
+{
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        if (Arr[i].BackColor == ScoreColor)
+        {   //And then changes it to black
+            Arr[i].BackColor = Color.Black;
+            break;
+        }
+    }
+
+    if (Arr[4].BackColor == Color.Black)
+    {
+        GameOn = false;
+        label_Start.Visible = true;
+        RestoreScore();
+        pb_Ball.Location = new Point(208, rng.Next(10, 190));
+        pb_Player.Location = new Point(3, 67);
+        pb_Enemy.Location = new Point(409, 67);
+        Round = 0;
+        label_Time.Visible = false;
+    }
+}
+    }
+}
---
+++ ping pong game/Form1.Designer (2).cs
@@ -0,0 +1,112 @@
+﻿namespace ping_pong_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer_Movement = new System.Windows.Forms.Timer(this.components);
+            this.pb_Player = new System.Windows.Forms.PictureBox();
+            this.pb_Enemy = new System.Windows.Forms.PictureBox();
+            this.pb_Ball = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Enemy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pb_Ball);
+            this.panel1.Location = new System.Drawing.Point(354, 156);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(143, 75);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pb_Player
+            // 
+            this.pb_Player.Image = global::ping_pong_game.Properties.Resources.fairy1;
+            this.pb_Player.Location = new System.Drawing.Point(684, 156);
+            this.pb_Player.Name = "pb_Player";
+            this.pb_Player.Size = new System.Drawing.Size(101, 101);
+            this.pb_Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Player.TabIndex = 2;
+            this.pb_Player.TabStop = false;
+            // 
+            // pb_Enemy
+            // 
+            this.pb_Enemy.Image = global::ping_pong_game.Properties.Resources.rocket;
+            this.pb_Enemy.Location = new System.Drawing.Point(34, 139);
+            this.pb_Enemy.Name = "pb_Enemy";
+            this.pb_Enemy.Size = new System.Drawing.Size(100, 92);
+            this.pb_Enemy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Enemy.TabIndex = 1;
+            this.pb_Enemy.TabStop = false;
+            this.pb_Enemy.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pb_Ball
+            // 
+            this.pb_Ball.Image = global::ping_pong_game.Properties.Resources.smallcar_png;
+            this.pb_Ball.Location = new System.Drawing.Point(3, 0);
+            this.pb_Ball.Name = "pb_Ball";
+            this.pb_Ball.Size = new System.Drawing.Size(140, 77);
+            this.pb_Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Ball.TabIndex = 0;
+            this.pb_Ball.TabStop = false;
+            this.pb_Ball.Click += new System.EventHandler(this.pb_Ball_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 486);
+            this.Controls.Add(this.pb_Player);
+            this.Controls.Add(this.pb_Enemy);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Enemy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pb_Ball;
+        private System.Windows.Forms.Timer timer_Movement;
+        private System.Windows.Forms.PictureBox pb_Enemy;
+        private System.Windows.Forms.PictureBox pb_Player;
+    }
+}
+
